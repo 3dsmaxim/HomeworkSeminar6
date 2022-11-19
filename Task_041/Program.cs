d14548cd0df5bf3994ca72640b7c29e80fb10137
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-11-19 10:45:06 +0300

    Задача 41

--- Task_041/Program.cs
+++ Task_041/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+
+
+
+int[] ArrayNums(string nums)
+{
+    string[] elemArr = nums.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] array = new int[elemArr.Length];
+    for (int i = 0; i < elemArr.Length; i++)
+    {
+        array[i] = int.Parse(elemArr[i]);
+    }
+    return array;
+}
+
+int CountPositive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите числа чтерез пробел: ");
+
+string elements = Console.ReadLine();
+
+int[] arrayTwo = ArrayNums(elements);
+
+Console.WriteLine($"В массиве [{String.Join(", ", arrayTwo)}] колличество чисел больше 0 равно {CountPositive(arrayTwo)}");
+
+
+// string[] nums = Console.ReadLine(string.Split(" ", StringSplitOptions.RemoveEmptyEntries));
